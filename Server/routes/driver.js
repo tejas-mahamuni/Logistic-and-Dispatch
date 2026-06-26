b546c7e0efe8ec3fcf151e0032fa7caa68198fc5
harshal-mekhe <captainharshal582@gmail.com>
2026-06-26 22:17:45 +0530

    Updated the route for driver.js

--- Server/routes/driver.js
+++ Server/routes/driver.js
@@ -6,19 +6,21 @@ const router = express.Router();
 const getConnection = require("../db");
 
 router.post("/", async (req, res) => {
+
     let connection;
+
     try {
         const {driverName, driverPhone, licenseNumber} = req.body;
 
         connection = await getConnection();
-        console.log("Driver insert payload:", { driverName, driverPhone, licenseNumber });
 
         await connection.execute(
             `INSERT INTO DRIVER(DRIVERNAME, PHONE, LICENSENUMBER)
-            VALUES(:1, :2, :3)`, 
+            VALUES (:1, :2, :3)`, 
             [driverName, driverPhone, licenseNumber],
             {autoCommit: true}
-        );
+);
+
 
         res.json({
             success: true,
@@ -26,21 +28,22 @@ router.post("/", async (req, res) => {
         });
     }
     catch(err) {
-        console.error("Driver Insert Error:", err);
         res.status(500).json({
             success: false,
-            message: err.message || "An error occured"
+            message: "An error occured"
         })
     }
     finally {
         if (connection) {
-            await connection.close();
+            await connection.close(); 
         }
     }
 });
 
 router.get("/:id", async (req, res) => {
+
     let connection;
+
     try {
         connection = await getConnection();
 
@@ -48,7 +51,6 @@ router.get("/:id", async (req, res) => {
             `SELECT DRIVERNAME, PHONE, LICENSENUMBER FROM DRIVER WHERE DRIVERID = :id`,
             [req.params.id]
         );
-
 
         if (result.rows.length === 0) {
             return res.status(404).json({
@@ -77,19 +79,105 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+router.get("/next/:id", async (req, res) => {
+
+    let connection;
+
+    try {
+        connection = await getConnection();
+
+        const result = await connection.execute(
+            `SELECT DRIVERID, DRIVERNAME, PHONE, LICENSENUMBER FROM DRIVER WHERE 
+            DRIVERID = (SELECT MIN(DRIVERID) FROM DRIVER WHERE DRIVERID > :1)`,
+            [req.params.id]
+        )
+
+        if (!result.rows || result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No more records found"
+            })
+        }
+
+        const row = result.rows[0];
+
+        res.json({
+            driverId: row[0],
+            driverName: row[1],
+            driverPhone: row[2],
+            licenseNumber: row[3]
+        });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+                message: "Record not found"
+        });
+    }
+    finally {
+        if (connection) {
+            await connection.close(); 
+        }
+    }
+});
+
+router.get("/previous/:id", async (req, res) => {
+
+    let connection;
+
+    try {
+        connection = await getConnection();
+
+        const result = await connection.execute(
+            `SELECT DRIVERID, DRIVERNAME, PHONE, LICENSENUMBER FROM DRIVER WHERE
+            DRIVERID = (SELECT MAX(DRIVERID) FROM DRIVER WHERE DRIVERID < :1)`,
+            [req.params.id]
+        )
+
+        if (!result.rows || result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No more records found"
+            });
+        };
+
+        const row = result.rows[0];
+
+        res.json({
+            driverId: row[0],
+            driverName: row[1],
+            driverPhone: row[2],
+            licenseNumber: row[3]
+        })
+    }
+    catch (err){
+        return res.status(404).json({
+                success: false,
+                message: "No more records found"
+            });
+    }
+    finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+});
+
 
 router.put("/:id", async (req, res) => {
 
-    const {driverName, driverPhone, licenseNumber} = req.body;
-    const connection = await getConnection();
+    const {driverName,driverPhone, licenseNumber} = req.body;
+    
+    let connection;
 
     try {
+        connection = await getConnection();
+
         const result = await connection.execute(
-            `UPDATE DRIVER SET DRIVERNAME = :1, PHONE = :2, LICENSENUMBER = :3 WHERE DRIVERID  = :4`,
+            `UPDATE DRIVER SET DRIVERNAME = :1, PHONE = :2, LICENSENUMBER = :3 WHERE DRIVERID = :4`,
             [driverName, driverPhone, licenseNumber, req.params.id],
             {autoCommit: true}
         );
-
 
         res.json({
             success: true,
@@ -110,34 +198,36 @@ router.put("/:id", async (req, res) => {
     }
 });
 
-router.delete("/:id", async(req,res) => {
-    let connection;
-    try {
-        connection = await getConnection();
+// router.delete("/:id", async(req,res) => {
 
-        await connection.execute(
-            `DELETE FROM DRIVER WHERE DRIVERID = :1`,
-            [req.params.id],
-            {autoCommit: true}
-        );
+//     let connection;
 
-        res.json({
-            success: true,
-            message: `Driver deleted with ${req.params.id}`
-        });
+//     try {
+//         connection = await getConnection();
 
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({
-            message: "Record not found"
-        });
-    }
-    finally {
-        if (connection) {
-            await connection.close(); 
-        }
-    }
-});
+//         await connection.execute(
+//             `DELETE FROM CUSTOMER WHERE CUSTOMERID = :1`,
+//             [req.params.id],
+//             {autoCommit: true}
+//         );
+
+//         res.json({
+//             success: true,
+//             message: `Customer deleted with ${req.params.id}`
+//         });
+
+//     }
+//     catch (err) {
+//         console.error(err);
+//         res.status(500).json({
+//             message: "Record not found"
+//         });
+//     }
+//     finally {
+//         if (connection) {
+//             await connection.close(); 
+//         }
+//     }
+// });
 
 module.exports = router;
